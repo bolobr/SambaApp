82e53ab3dd5696d686bd7b21b77fb1e1bdb66740
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2015-06-04 15:01:10 -0300

    Fixing Instance Object tests

--- App/tests/models/video.spec.js
+++ App/tests/models/video.spec.js
@@ -1,32 +1,45 @@
 var Video = require('../../api/models/Video'),
     sinon = require('sinon'),
-    assert = require('assert');
+    assert = require('assert'),
+    wolfpack = require('wolfpack');
+var video_inst = wolfpack('/api/models/Video');
 
 describe('Model Video', function () {
-  v = Video.create({
-    name: "Teste"
-  });
+
   //Name Validation
   describe("Name Attribute", function(){
-    it("it should have a name ", function(){
+    it("it should have a name attribute", function(){
       assert.equal(Video.attributes.name.type, 'string');
     });
-    it("it should be required", function(){
-      assert.equal(Video.attributes.name.required, true);
-    })
   })
-
   //Path Validation
   describe("Paths", function(){
     it("should have an original video path", function(){
-      assert.equal(typeof Video.original_path, 'string');
+      assert.equal(typeof Video.attributes.original_video_path, 'string');
     });
     it("should have an encoded video path", function(){
-      assert.equal(typeof Video.encoded_video_path, 'string');
-    });
-    it("should not be equal", function(){
-      assert.notEqual(Video.encoded_video_path, Video.original_path);
+      assert.equal(typeof Video.attributes.encoded_video_path, 'string');
     });
   });
+  describe("Functions", function(){
+    it("shoud have a beforeCreate", function(){
+      assert.equal(typeof Video.beforeCreate, 'function');
+    })
+  })
 
+
+});
+
+
+describe('Instance of Video', function(){
+  v = video_inst.create({
+    name: "teste",
+    original_path: "/home/teste",
+    encoded_video_path: "/home/teste"
+  }, function(){
+    it("Should not have same path for both", function(){
+      assert.notEqual(v.original_path, v.encoded_video_path);
+
+    });
+  });
 });
