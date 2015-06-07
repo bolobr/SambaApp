bfe6ea804f83cb2f290b6d25f21ad9c5d0690a08
Guilherme Cordeiro <guilherme.cscordeiro@gmail.com>
2015-06-07 11:00:27 -0300

    New version of angular

---
+++ assets/js/uploadController.js
@@ -0,0 +1,20 @@
+angular
+.module('uploadModule')
+.controller('uploadController',
+['$scope', '$http', 'toastr', function($scope, $http, toastr){
+  $scope.dirty = false;
+  $scope.check_name = function(){
+    $http.post('/check_name',{
+      name: $scope.uploadForm.name
+    }).then(function onSuccess(msg){
+      if(msg.data == true){
+        console.log("Name Taken");
+        $scope.dirty = true;
+      } else{
+        $scope.dirty = false;
+      }
+    }).catch(function onError(response){
+    });
+  }
+
+}]);
